81fd00f6e801204dc4b498406c182c3207fef28d
Men328 <22010328@st.phenikaa-uni.edu.vn>
2024-10-17 10:22:00 +0700

    add lession_1.hmtl

---
+++ lession_1.js
@@ -0,0 +1,12 @@
+alert("hello world inside index.id file")
+
+let number = 10; //khai báo biến 
+// (cấu trúc: từ khóa khai báo biến + tên biến = giá trị của biến)
+
+number; //gọi lại giá trị đang nằm trong 1 biến
+
+number = 20; //gọi lại giá trị và gán một giá trị mới vào biến
+
+let hens = 20;
+
+hens = hens + 20; //lưu trữ giá trị mới = 40.
